af9320fd82df22ce8c2f8ce0ab152e5eeeb74314
sophietao <sophietao@10.232.158.144>
2025-01-22 14:03:49 -0600

    Leap year

--- src/studio1/LeapYear.java
+++ src/studio1/LeapYear.java
@@ -12,6 +12,7 @@ public class LeapYear {
 		boolean isLeapYear = ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
 		System.out.print(year + " is a leap year: " + isLeapYear);
 		 
+		 
 	}
 
 }
